54f6585009ea30054a3ea6696c5d06765b719397
FefeJones <103066827+FefeJones@users.noreply.github.com>
2022-09-08 10:01:43 -0300

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,25 @@
+class Game{
+    private int app_id;
+    private String name;
+    private Data release_date;
+    private String owners;
+    private int age;
+    private Float price; 
+    private int dlcs; 
+    private String vet[] = new String[];
+    private boolean windows; 
+    private boolean mac; 
+    private boolean linux;
+    private float upvotes;
+    private int avg_pt; 
+    private String developers;
+    private String[] genres;
+
+
+
+
+
+    public static void main(String[] args){
+
+    }
+}
